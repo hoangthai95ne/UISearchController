5a90b219c090c698e2eadc6baa4fed5b1586e90b
IM Hoang Thai <hoangthai95ne@gmail.com>
2016-03-14 09:33:09 +0700

    Initial Commit

---
+++ SearchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchBar
+//
+//  Created by Hoàng Thái on 3/14/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchBar
+//
+//  Created by Hoàng Thái on 3/14/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
